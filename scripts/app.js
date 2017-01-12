ee6a98e221c2e866c405f5e2c12f7a12246a52b1
Ingaaa <pire.inga@gmail.com>
2017-01-12 02:39:23 +0200

    Changes

--- scripts/app.js
+++ scripts/app.js
@@ -4,6 +4,10 @@ app.config(['$routeProvider', '$locationProvider',
         $routeProvider
             .when("/", {
                 templateUrl: "/pages/zinas.html"
+            }).when("/zinas", {
+                templateUrl: "/pages/zinas.html"
+            }).when("/zinas/:zina", {
+                templateUrl: "/pages/page.html"
             }).when("/fakultates", {
                 templateUrl: "/pages/fakultates.html"
             }).when("/studentiem", {
@@ -12,20 +16,79 @@ app.config(['$routeProvider', '$locationProvider',
                 templateUrl: "/pages/bibliotekas.html"
             });
     }]);
-app.controller("Ctrl", function($scope) {
-    $scope.products = ["Milk", "Bread", "Cheese"];
-});
-/*app.controller('RecipesController', [
-    '$scope', '$rootScope', '$http',
-    function ($scope, $rootScope, $http) {
-        $http.get("/recipes.json")
-            .then(function (data) {
-                $rootScope.recipes = data.data.recipes;
-            }, function () {
 
-            });
+app.controller('ZinasController', [
+    '$scope', '$rootScope',
+    function ($scope, $rootScope) {
+        $rootScope.news = [
+            {
+                id: 0,
+                title: "Ziemassvētku vecītis - ir! Dekāni sveic visus Ziemassvētkos!",
+                img: "Pictures/ZSvetki.jpg",
+                display: "large",
+            },
+            {
+                display: "multiple",
+                smallLeft: {
+                    id: 1,
+                    title: "LU kopā ar čekas zinātniskās izpētes komisiju atvērs rakstu “Lielais brālis tevi vēro” 3. sējumu",
+                    imgId: "img1"
+                },
+                smallRight: {
+                    id: 2,
+                    title: "Mērķis – mainīt un ietekmēt dzīvi. Latvijas Universitātes nākamie 100 gadi zinātnē",
+                    imgId: "img2"
+                }
+            },
+            {
+                display: "multiple",
+                smallLeft: {
+                    id: 3,
+                    title: "1. decembrī Radio NABA ēterā būs dzirdama īpaša 14. dzimšanas dienas svētku programma",
+                    imgId: "img3"
+                },
+                smallRight: {
+                    id: 4,
+                    title: "Latvijas Universitāte sāk ilgtermiņa sadarbību ar Eiropas Ājurvēdas akadēmiju Vācijā",
+                    imgId: "img4"
+                }
+            },
+            {
+                display: "multiple",
+                smallLeft: {
+                    id: 5,
+                    title: "LU rektors Indriķis Muižnieks uzklausa bijušo rektoru idejas attīstībai",
+                    imgId: "img5"
+                },
+                smallRight: {
+                    id: 6,
+                    title: "Iznācis Latvijas Universitātes žurnāla “Alma Mater” rudens numurs",
+                    imgId: "img6"
+                }
+            },
+            {
+                display: "multiple",
+                smallLeft: {
+                    id: 7,
+                    title: "Studentu dalība Latvijas Brīvības cīņās - zelta burtiem rakstīta lappuse LU vēsturē",
+                    imgId: "img7"
+                },
+                smallRight: {
+                    id: 8,
+                    title: "Latvijas Universitāte atzīmē tās pamatlicēja Ernesta Felsberga 150 gadu jubileju",
+                    imgId: "img8"
+                }
+            },
+            {
+                id: 9,
+                title: "Latvijas Universitāte sveic valsts 98. gadadienā!",
+                img: "Pictures/Latvija.jpg",
+                display: "large",
+            }
+        ];
+        $scope.news = $rootScope.news;
     }
-]);*/
+]);
 
 /*app.controller('RecipeController', [
     '$scope', '$rootScope', '$http', '$routeParams',
