1d1ce7257aefe3fd2aa6b75699d156a4dfb0ae2e
Ingaaa <pire.inga@gmail.com>
2017-01-14 00:02:06 +0200

    Almost finished both MD1 and MD2

--- MD1/scripts/app.js
+++ MD1/scripts/app.js
@@ -10,31 +10,132 @@ app.config(['$routeProvider', '$locationProvider',
                 templateUrl: "/MD1/pages/shop.html"
             }).when("/inspire", {
                 templateUrl: "/MD1/pages/inspire.html"
-            }).when("/story", {
-                templateUrl: "/MD1/pages/story.html"
-            }).when("/user", {
-                templateUrl: "/MD1/pages/user.html"
-            }).when("/cart", {
-                templateUrl: "/MD1/pages/cart.html"
             }).when("/about", {
                 templateUrl: "/MD1/pages/about.html"
+            }).when("/contact", {
+                templateUrl: "/MD1/pages/contact.html"
             });
     }]);
 
-/*app.run(['$rootScope',
+app.run(['$rootScope',
     function ($rootScope) {
-        $rootScope.news = news;
-        $rootScope.newsText = newsText;
-        $rootScope.fakultates = fakultates;
-        $rootScope.bibliotekas = bibliotekas;
-        $rootScope.studentiem = studentiem;
+        $rootScope.products = products;
+        $rootScope.inspires = inspires;
+        $rootScope.values = values;
     }
 ]);
 
-app.controller('ZinasController', [
+app.controller('ShopController', [
     '$scope', '$rootScope',
     function ($scope, $rootScope) {
-        $scope.news = $rootScope.news;
+        $scope.products = $rootScope.products;
     }
 ]);
-*/
+app.controller('InspireController', [
+    '$scope', '$rootScope',
+    function ($scope, $rootScope) {
+        $scope.inspires = $rootScope.inspires;
+    }
+]);
+app.controller('AboutController', [
+    '$scope', '$rootScope',
+    function ($scope, $rootScope) {
+        $scope.values = $rootScope.values;
+    }
+]);
+
+var products = [
+    {
+        id: 0,
+        title: "Roses",
+        img: "Pictures/Roses.jpg"
+    },
+    {
+        id: 1,
+        title: "Tulips",
+        img: "Pictures/Tulips.jpg"
+    },
+    {
+        id: 2,
+        title: "Orchids",
+        img: "Pictures/Orchids.jpg"
+    },
+    {
+        id: 3,
+        title: "Gerberas",
+        img: "Pictures/Gerberas.jpg"
+    },
+    {
+        id: 4,
+        title: "Dahlias",
+        img: "Pictures/Dahlias.jpg"
+    },
+    {
+        id: 5,
+        title: "Peonies",
+        img: "Pictures/Peonies.jpg"
+    },
+    {
+        id: 6,
+        title: "Dasies",
+        img: "Pictures/Dasies.jpg"
+    },
+    {
+        id: 7,
+        title: "Others",
+        img: "Pictures/Others.jpg"
+    }
+];
+
+var inspires = [
+    {
+        title: "Anniversary",
+        img: "Pictures/Insp1.jpg"
+    },
+    {
+        title: "101 roses",
+        img: "Pictures/Insp2.jpg"
+    },
+    {
+        title: "Flower buckets",
+        img: "Pictures/Insp3.jpg"
+    },
+    {
+        title: "Birthday",
+        img: "Pictures/Insp4.jpg"
+    },
+    {
+        title: "Floristics",
+        img: "Pictures/Insp5.jpg"
+    },
+    {
+        title: "Wedding",
+        img: "Pictures/Insp6.jpg"
+    },
+    {
+        title: "Bouquets",
+        img: "Pictures/Insp7.jpg"
+    },
+];
+var values = [
+    {
+        title: "Fresh from the field",
+        img: "Icons/sprout.png"
+    },
+    {
+        title: "Field-to-door",
+        img: "Icons/house-black-silhouette-without-door.png"
+    },
+    {
+        title: "Thoughtfully convenient",
+        img: "Icons/thought-bubble-.png"
+    },
+    {
+        title: "Service delivered",
+        img: "Icons/delivery-truck.png"
+    },
+    {
+        title: "Discover the great quality",
+        img: "Icons/quality.png"
+    },
+];
